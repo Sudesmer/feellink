d3ec63d3e5e5afd3f724b491685fe23862d04d8a
Sudesmer <sudesmer001@gmail.com>
2025-10-25 17:03:18 +0300

    Auto-commit: 2025-10-25 17:03:49

---
+++ config/prisma.js
@@ -0,0 +1,15 @@
+const { PrismaClient } = require('../prisma/@prisma/client');
+
+// Singleton pattern - tek bir Prisma instance kullan
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+module.exports = prisma;
